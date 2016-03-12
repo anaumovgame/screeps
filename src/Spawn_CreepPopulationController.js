df50685a39a0e0e70ee2daa3d4a0297b8f9062a8
anaumovgame <game@anaumov.com>
2016-03-12 23:44:47 +0300

    Auto-Commit

--- src/Spawn_CreepPopulationController.js
+++ src/Spawn_CreepPopulationController.js
@@ -11,3 +11,21 @@ module.exports = function(spawnName)
         }
     }
 };
+
+function SetDefaultPopulation(spawnName)
+{
+    var workerMax = 2;
+    var builderMax = 1;
+    var guardMax = 0;
+
+    SetPopulation(spawnName, workerMax, builderMax, guardMax);
+}
+
+function SetPopulation(spawnName, workerMax, builderMax, guardMax)
+{
+    console.log(spawnName + " : SetPopulation : " + workerMax + ", " + builderMax + ", " + guardMax);
+
+    Game.spawns[spawnName].memory.workerMax = workerMax;
+    Game.spawns[spawnName].memory.builderMax = builderMax;
+    Game.spawns[spawnName].memory.guardMax = guardMax;
+}
