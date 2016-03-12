2f045932043a79d05e3eaece36c23d62f929ff86
anaumovgame <game@anaumov.com>
2016-03-13 00:43:02 +0300

    Auto-Commit

--- src/Spawn_CreepPopulationController.js
+++ src/Spawn_CreepPopulationController.js
@@ -2,14 +2,20 @@ module.exports = function(spawnName)
 {
     console.log("Manage population spawn: " + spawnName);
 
-
-
     for (var creepName in Game.creeps)
     {
         var creep = Game.creeps[creepName];
         if (creep.memory.spawnName == spawnName)
         {
-            console.log(spawnName + " : " + creepName);
+
         }
+    }
+}
+
+function isMiner(creep)
+{
+    if (creep.memory.class == "Miner")
+    {
+        
     }
 }
