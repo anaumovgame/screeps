622dfa3f8f956bb90732a01a4af8d38205b0eb53
anaumovgame <game@anaumov.com>
2016-03-12 23:00:50 +0300

    Auto-Commit

--- src/CreepManager.js
+++ src/CreepManager.js
@@ -3,6 +3,11 @@ module.exports = function(spawnName)
     console.log("Creep-manage spawn: " + spawnName);
     creeps = Game.creeps.filter(function(x) {
        if (x.spawnName = spawnName)
-        {return true} else {return false};
+       {
+            return true
+       } else
+       {
+           return false
+       };
     });
 }
