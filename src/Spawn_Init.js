999059e5cd48fccb6c6be5f4194c02df8dc4fd7f
anaumovgame <game@anaumov.com>
2016-03-13 00:24:43 +0300

    Auto-Commit

--- src/Spawn_Init.js
+++ src/Spawn_Init.js
@@ -2,7 +2,6 @@ module.exports = function(spawnName, workerMax, builderMax, guardMax)
 {
 //    if (Game.spawns[spawnName].memory.isInit != true)
     {
-        Game.spawns[spawnName].prototype.say.call(null, "Population : " + workerMax + ", " + builderMax + ", " + guardMax);
         console.log("Spawn init: " + spawnName + " : Population : " + workerMax + ", " + builderMax + ", " + guardMax);
 
         Game.spawns[spawnName].memory.workerMax = workerMax;
