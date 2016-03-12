a84fccc1f585d2098c9fcb6c75377b174cd8d522
anaumovgame <game@anaumov.com>
2016-03-12 22:37:11 +0300

    Auto-Commit

--- src/SpawnController.js
+++ src/SpawnController.js
@@ -2,6 +2,6 @@ module.exports = function(test)
 {
     for (var name in Game.spawns)
     {
-        console.log(name);
+        console.log();
     }
 }
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ var SpawnController = require("SpawnController");
 
 module.exports.loop = function()
 {
+    Memory.setAttribute()
     SpawnController();
 }
