ac055d2c43f63b4f38be17efec92493f330088b2
anaumovgame <game@anaumov.com>
2016-03-12 23:44:37 +0300

    Auto-Commit

--- src/main.js
+++ src/main.js
@@ -12,21 +12,3 @@ function InitMemory()
     SetDefaultPopulation("S1");
     SetDefaultPopulation("S2");
 }
-
-function SetDefaultPopulation(spawnName)
-{
-    var workerMax = 2;
-    var builderMax = 1;
-    var guardMax = 0;
-
-    SetPopulation(spawnName, workerMax, builderMax, guardMax);
-}
-
-function SetPopulation(spawnName, workerMax, builderMax, guardMax)
-{
-    console.log(spawnName + " : SetPopulation : " + workerMax + ", " + builderMax + ", " + guardMax);
-
-    Game.spawns[spawnName].memory.workerMax = workerMax;
-    Game.spawns[spawnName].memory.builderMax = builderMax;
-    Game.spawns[spawnName].memory.guardMax = guardMax;
-}
