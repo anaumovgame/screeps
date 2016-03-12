a73ed642938d0e8fd8a792a93f303085b108e4a9
anaumovgame <game@anaumov.com>
2016-03-13 02:50:04 +0300

    Auto-Commit

--- src/Creep_Miner.js
+++ src/Creep_Miner.js
@@ -1,10 +1,10 @@
 var CreepConst = require("Creep_Const");
-var ActionMine = require("Action_Const");
+var ActionConst = require("Action_Const");
 var ActionMine = require("Action_Mine");
 
 module.exports = {
     name : "Miner",
     className : CreepConst.Creep_Miner,
     body : [WORK, CARRY, MOVE],
-    //action : function(){console.log("action")}.toSource(),
+    action : ActionConst.Action_Mine;
 }
