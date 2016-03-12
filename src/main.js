cb85ee40f0c31fa881005a99173651071b05016a
anaumovgame <game@anaumov.com>
2016-03-12 22:43:55 +0300

    Auto-Commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ var SpawnController = require("SpawnController");
 
 module.exports.loop = function()
 {
-    
+    InitMemory();
     SpawnController();
+}
+
+function InitMemory()
+{
+    if (Memory.getAttribute("isInit") == false)
+    {
+        Memory.setAttribute("isInit", true);
+    }
 }
