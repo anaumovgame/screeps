4006e73be908e340d825c10e93a7157cc45ae597
anaumovgame <game@anaumov.com>
2016-03-13 00:37:40 +0300

    Auto-Commit

--- src/Creep_Classes.js
+++ src/Creep_Classes.js
@@ -1,6 +1,6 @@
 module.exports = {
     Miner        : require("Creep_Miner"),
     Worker       : require("Creep_Worker"),
-    Guard      : require("Creep_Guard"),
+    Guard        : require("Creep_Guard"),
     Healer       : require("Creep_Healer")
 };
