b5bb5e246194cf0d69b19e28b001242c1b5f268c
anaumovgame <game@anaumov.com>
2016-03-13 00:10:22 +0300

    Auto-Commit

--- src/Spawn_Init.js
+++ src/Spawn_Init.js
@@ -1,11 +1,12 @@
 module.exports = function(spawnName, workerMax, builderMax, guardMax)
 {
-    if (Game.spawns[spawnName].memory.isInit != true)
-    console.log("Spawn init: " + spawnName + " : Population : " + workerMax + ", " + builderMax + ", " + guardMax);
+    if (Game.spawns[spawnName].memory.isInit != true) {
+        console.log("Spawn init: " + spawnName + " : Population : " + workerMax + ", " + builderMax + ", " + guardMax);
 
-    Game.spawns[spawnName].memory.workerMax = workerMax;
-    Game.spawns[spawnName].memory.builderMax = builderMax;
-    Game.spawns[spawnName].memory.guardMax = guardMax;
+        Game.spawns[spawnName].memory.workerMax = workerMax;
+        Game.spawns[spawnName].memory.builderMax = builderMax;
+        Game.spawns[spawnName].memory.guardMax = guardMax;
 
-    Game.spawns[spawnName].memory.isInit = true;
+        Game.spawns[spawnName].memory.isInit = true;
+    }
 }
