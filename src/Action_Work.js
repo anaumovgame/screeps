abe45638ecfe73782cb670e4e8bc8bcd785fb024
anaumovgame <game@anaumov.com>
2016-03-13 05:00:42 +0300

    Auto-Commit

--- src/Action_Work.js
+++ src/Action_Work.js
@@ -5,8 +5,12 @@ module.exports = function(creep)
     var creepSpawnName = creep.memory.spawnName;
     var spawn = Game.spawns[creepSpawnName];
     var sites = creep.room.find(FIND_CONSTRUCTION_SITES);
-    if (creep.build(sites[0]) == -9)
+    if (creep.energy > 0) {
+        if (creep.build(sites[0]) == -9) {
+            creep.moveTo(sites[0]);
+        }
+    } else
     {
-        creep.moveTo(sites[0]);
+        creep.moveTo(spawn);
     }
 }
