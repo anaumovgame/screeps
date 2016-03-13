05697f6e83649dc1e4db277dcb580f3bd6ff0076
anaumovgame <game@anaumov.com>
2016-03-13 22:43:58 +0300

    Auto-Commit

--- src/Spawn_Controller.js
+++ src/Spawn_Controller.js
@@ -1,6 +1,6 @@
 var CreepPopulationController = require("Spawn_CreepPopulationController");
 var CreepUpgrader = require("Spawn_CreepUpgrader");
-var CreepUpgrader = require("Spawn_CreepRenewer");
+var CreepRenewer = require("Spawn_CreepRenewer");
 
 module.exports = function()
 {
