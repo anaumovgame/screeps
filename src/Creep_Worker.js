53d2566770d48cd3d1c407a826c82f39d4c0e14d
anaumovgame <game@anaumov.com>
2016-03-13 04:56:26 +0300

    Auto-Commit

--- src/Creep_Worker.js
+++ src/Creep_Worker.js
@@ -1,3 +1,9 @@
-/**
- * Created by Naumov on 13.03.2016.
- */
+var CreepConst = require("Creep_Const");
+var ActionConst = require("Action_Const");
+
+module.exports = {
+    name : "Worker",
+    className : CreepConst.Creep_Miner,
+    body : [WORK, CARRY, MOVE],
+    actionName : ActionConst.Action_Mine,
+}
