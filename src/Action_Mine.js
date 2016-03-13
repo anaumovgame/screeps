efc9d642b7223beaf1e4c79f2bb3e8c1e07b698f
anaumovgame <game@anaumov.com>
2016-03-13 03:40:36 +0300

    Auto-Commit

--- src/Action_Mine.js
+++ src/Action_Mine.js
@@ -1,18 +1,22 @@
 module.exports = function(creep)
 {
     //console.log("Mine");
-    var flags = creep.room.find(FIND_FLAGS);
-//    var path = PathFinder.search(creep.pos)
+
+
 }
 
 function selectNearestFlag(resource_type)
 {
+    var flagName;
+    var wayLength = 999999;
+    
     var creepSpawnName = creep.memory.spawnName;
     var spawn = Game.spawns[creepSpawnName];
 
+    var flags = creep.room.find(FIND_FLAGS);
     for (var flagName in flags)
     {
-
+        var path = PathFinder.search(creep.pos)
     }
 
     return flagName;
