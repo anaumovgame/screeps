726709767e46e2652fc1b5a542ba3a4763cbee1b
anaumovgame <game@anaumov.com>
2016-03-13 21:12:59 +0300

    Auto-Commit

--- src/Spawn_CreepUpgrader.js
+++ src/Spawn_CreepUpgrader.js
@@ -1,12 +1,20 @@
+var CreepConst = require("Creep_Const");
+var CreepClasses = require("Creep_Classes");
+
 module.exports = function(){
     for (var creepName in Game.creeps)
     {
         var creep = Game.creeps[creepName];
+
+        //ќпредел€ю текущее сост€ние body крипа
         var bodyParts = [];
         for (var bodyPart in creep.body)
         {
             bodyParts.push(creep.body[bodyPart].type);
         }
         console.log(creepName + " : " + bodyParts);
+
+        //ќпредел€ю каким должно быть тело крипа
+        var finalBody = CreepClasses
     }
 }
