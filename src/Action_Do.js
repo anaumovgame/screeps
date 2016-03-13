c430be61b11e38cbee5747746abf229aad84439a
anaumovgame <game@anaumov.com>
2016-03-13 03:04:55 +0300

    Auto-Commit

--- src/Action_Do.js
+++ src/Action_Do.js
@@ -1,6 +1,9 @@
 var ActionMine = require("Action_Mine");
+var ActionConst = require("Action_Const");
 
 module.exports = function(creep) {
     console.log("Do: " + creep.memory.actionName);
-
+    switch (creep.memory.actionName){
+        case Action
+    }
 }
