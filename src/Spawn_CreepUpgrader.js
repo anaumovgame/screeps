6c4cd031eacb495d0fdf7e04dc69219492259597
anaumovgame <game@anaumov.com>
2016-03-13 21:00:15 +0300

    Auto-Commit

--- src/Spawn_CreepUpgrader.js
+++ src/Spawn_CreepUpgrader.js
@@ -1,3 +1,8 @@
 module.exports = function(){
-
+    for (var creepName in Game.creeps)
+    {
+        creep = Game.creeps[creepName];
+        console.log(creep.body);
+        //if (creep.memory.className)
+    }
 }
