e037991a46459cd167657893bdb0799d1d78212c
anaumovgame <game@anaumov.com>
2016-03-13 04:34:39 +0300

    Auto-Commit

--- src/Action_Mine.js
+++ src/Action_Mine.js
@@ -9,7 +9,7 @@ module.exports = function(creep)
     {
         var enegrySource = creep.room.find(FIND_SOURCES, {filter : {pos : nearestFlag.pos} });
         var asdsa = creep.harvest(enegrySource.name);
-        console.log(enegrySource);
+        console.log(enegrySource.name);
         if (creep.harvest(enegrySource) == -6)
         {
             creep.moveTo(enegrySource);
