0d2583f0a7e2ca16b67dbfd5b0f4ecc4b16508b0
anaumovgame <game@anaumov.com>
2016-03-15 21:50:46 +0300

    Auto-Commit

--- src/Tower_Controller.js
+++ src/Tower_Controller.js
@@ -1,1 +1,28 @@
+module.exports = function()
+{
+//TODO: ВСЁ ПЕРЕДЕЛАТЬ!!!
 
+}
+
+//Поиск стены с меньшим ХП
+function selectNearestExtensionWithEnergy(creep)
+{
+    var nearestExtension;
+    var wayLength = 9999999;
+
+    //Перебираю все флаги в комнате
+    var walls = creep.room.find(FIND_MY_STRUCTURES, {filter: { structureType: "wall" }});
+    for (var wallNum in walls) {
+        var wall = walls[wallNum];
+            //Измеряю HP стены
+            var wallHp = wall.hits;
+            //Сохраняю башню с наименьшим ХП
+            if (way.path.length < wayLength) {
+                nearestExtension = wall;
+                wayLength = way.path.length;
+            }
+        }
+    }
+
+    return nearestExtension;
+}
