86f4c38b5f715df3bb0a7e9b4aa73648f9d68018
anaumovgame <game@anaumov.com>
2016-03-19 10:26:19 +0300

    Auto-Commit

--- src/Action_Mine.js
+++ src/Action_Mine.js
@@ -135,3 +135,29 @@ function selectNearestFlag(spawn, flag_type)
 
     return nearestFlag;
 }
+
+//Поиск ближайшей к спавну свободной энергии
+function selectNearestFlag(spawn, flag_type)
+{
+    var nearestFlag;
+    var wayLength = 999999;
+
+    //Перебираю все флаги в комнате
+    var flags = spawn.room.find(FIND_FLAGS);
+    for (var flagNum in flags)
+    {
+        var flag = flags[flagNum];
+        //Если флаг соответствует ресурсу
+        if (flag.name.indexOf(flag_type) > -1) {
+            //Измеряю путь
+            var way = PathFinder.search(spawn.pos, flag.pos);
+            //Сохраняю наименьший путь
+            if (way.path.length < wayLength) {
+                nearestFlag = flag;
+                wayLength = way.path.length;
+            }
+        }
+    }
+
+    return nearestFlag;
+}
