180ad0b4b30e4902194099348fb5cc3bd0830f96
anaumovgame <game@anaumov.com>
2016-03-20 13:20:51 +0300

    Auto-Commit

--- src/Creep_Worker.js
+++ src/Creep_Worker.js
@@ -8,7 +8,7 @@ module.exports = {
         [WORK, WORK, CARRY, CARRY, MOVE], //1
         [WORK, WORK, CARRY, CARRY, MOVE, MOVE], //2
         [WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE], //3
-        [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], //4
+        [WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE], //4
         [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE], //5
     ],
     actionName : ActionConst.Action_Work,
