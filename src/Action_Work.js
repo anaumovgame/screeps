5b71d488e9628745eb812a7932e3bb54d09b9b0e
anaumovgame <game@anaumov.com>
2016-03-20 14:33:50 +0300

    Auto-Commit

--- src/Action_Work.js
+++ src/Action_Work.js
@@ -26,7 +26,7 @@ module.exports = function(creep)
             }
         } else {
             if (isSpawnWaitCreep(spawn) == false) {
-                if (spawn.energy >= 50) {
+                if (spawn.energy >= creep.energyCapacity) {
                     if (spawn.transferEnergy(creep) == -9) {
                         creep.moveTo(spawn);
                     }
