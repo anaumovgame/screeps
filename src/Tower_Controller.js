fb6b9df69ec768bb55919d7b3e289fe76c26fa3f
anaumovgame <game@anaumov.com>
2016-03-20 12:03:58 +0300

    Auto-Commit

--- src/Tower_Controller.js
+++ src/Tower_Controller.js
@@ -14,24 +14,24 @@ module.exports = function()
 //Поиск структуры, которую надо чинить
 function selectLowHPStructure(room, tower)
 {
-    var lowHitWall = null;
+    var lowHitStructure = null;
     var minWallHP = 999999999;
 
 
     //Перебираю все флаги в комнате
-    var walls = room.find(FIND_STRUCTURES, {filter: { structureType: STRUCTURE_WALL }});
-    for (var wallNum in walls) {
-        var wall = walls[wallNum];
+    var structures = room.find(FIND_STRUCTURES, {filter: { structureType: STRUCTURE_WALL }});
+    for (var structureNum in structures) {
+        var wall = structures[structureNum];
         //Измеряю HP стены
         var wallHP = wall.hits;
         //Сохраняю башню с наименьшим ХП
         if (wallHP < minWallHP) {
-            lowHitWall = wall;
+            lowHitStructure = wall;
             minWallHP = wallHP;
         }
     }
 
-    return lowHitWall;
+    return lowHitStructure;
 }
 
 
