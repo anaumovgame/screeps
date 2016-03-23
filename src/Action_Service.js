72588f55cead10beb7b839e10423482c71d26e21
anaumovgame <game@anaumov.com>
2016-03-23 22:48:37 +0300

    Auto-Commit

--- src/Action_Service.js
+++ src/Action_Service.js
@@ -4,33 +4,7 @@ var CreepConst = require("Creep_Const");
 module.exports = function(creep) {
     var creepSpawnName = creep.memory.spawnName;
     var spawn = Game.spawns[creepSpawnName];
-
-}
-
-//Поиск ближайшего к спауну extension
-function selectNearestEmptyExtension(spawn)
-{
-    var nearestExtension = null;
-    var wayLength = 999999;
-
-    //Перебираю все флаги в комнате
-    var extensions = spawn.room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_EXTENSION }});
-    for (var extensionNum in extensions) {
-        var extension = extensions[extensionNum];
-
-        //Если в хранилище есть место
-        if (extension.energy < extension.energyCapacity) {
-            //Измеряю путь
-            var way = PathFinder.search(spawn.pos, extension.pos);
-            //Сохраняю наименьший путь
-            if (way.path.length < wayLength) {
-                nearestExtension = extension;
-                wayLength = way.path.length;
-            }
-        }
-    }
-
-    return nearestExtension;
+    var extension = selectNearestExtensionWithoutEnergy(creep);
 }
 
 //Поиск ближайшего к крипу extension который можно заполнить энергией
