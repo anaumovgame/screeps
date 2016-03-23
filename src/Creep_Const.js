66b0740939a35779e373e3f500c6269290171983
anaumovgame <game@anaumov.com>
2016-03-23 22:37:58 +0300

    Auto-Commit

--- src/Creep_Const.js
+++ src/Creep_Const.js
@@ -2,6 +2,7 @@ module.exports = {
     //Names
     Creep_Miner   : "Creep_Miner",
     Creep_Worker  : "Creep_Worker",
+    Creep_Servant   : "Creep_Servant",
     Creep_Guard   : "Creep_Guard",
     Creep_Healer  : "Creep_Healer",
 
