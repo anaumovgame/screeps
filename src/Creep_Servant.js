f9496e229ac97ced34215bd243326290e4372904
anaumovgame <game@anaumov.com>
2016-03-23 22:38:52 +0300

    Auto-Commit

--- src/Creep_Servant.js
+++ src/Creep_Servant.js
@@ -1,3 +1,30 @@
-/**
- * Created by Naumov on 23.03.2016.
- */
+var CreepConst = require("Creep_Const");
+var ActionConst = require("Action_Const");
+
+module.exports = {
+    name : "Servant",
+    className : CreepConst.Creep_Worker,
+    body : [[WORK, WORK, CARRY, MOVE], //0
+        [WORK, WORK, CARRY, CARRY, MOVE], //1
+        [WORK, WORK, CARRY, CARRY, MOVE, MOVE], //2
+        [WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE], //3
+        [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE], //4
+        [WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE], //5
+        [WORK, WORK, WORK, WORK, WORK, CARRY, MOVE], //6
+        [WORK, WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE], //7
+        [WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE], //8
+        [WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE], //9
+        [WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], //10
+        [WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE], //11
+        [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE], //12
+        [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE], //13
+        [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE], //14
+        [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE], //15
+        [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE], //16
+        [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE], //17
+        [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE], //18
+        [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE], //19
+        [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE], //20
+    ],
+    actionName : ActionConst.Action_Work,
+}
